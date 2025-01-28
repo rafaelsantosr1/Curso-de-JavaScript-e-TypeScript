087ca2a706b637ae20ffae4195738b314da20e7c
rfsantosr <rafaelhulk2@gmail.com>
2025-01-28 17:19:02 -0300

    aula 18

---
+++ aula 17/index.js
@@ -0,0 +1,40 @@
+const raiz = function (n){
+    return n ** 0.5;
+};
+
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
+
+
+
+/*
+function soma(x = 1, y = 1){
+    const resultado = x + y;
+    return resultado;
+}
+
+const resultado = soma();
+console.log(resultado);
+
+*/
+
+
+/*
+function soma(x, y){
+    const resultado = x + y;
+    return resultado;
+}
+
+const resultado = soma(2, 2);
+console.log(resultado);
+*/
+
+/*
+function saudacao(nome) {
+  return `Bom dia ${nome}!`;
+}
+
+const variavel = saudacao('Fulano');
+console.log(variavel);
+*/
