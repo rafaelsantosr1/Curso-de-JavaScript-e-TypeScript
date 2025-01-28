4c76146ae49d46f1e259d30c95b6b85fb73eac21
rfsantosr <rafaelhulk2@gmail.com>
2025-01-28 09:36:33 -0300

    aula 15

---
+++ aula 14/index.js
@@ -0,0 +1,19 @@
+//IEEE 753-2008
+
+let num1 = 0.7; //number
+let num2 = 0.1; //number
+
+num1 += num2;
+console.log(num1 + num2);
+
+// console.log(num1.toString() + num2);
+// num1 = num1.toString();
+// console.log(typeof num1);
+// console.log(num1.toString(2));
+// console.log(num1.toFixed(2)); //arredonda para 2 casas decimais
+
+// console.log(Number.isInteger(num1)); //informa se é inteiro ou não
+
+// let temp = num1 * 'Ola';
+// console.log(Number.isNaN(temp)); //retorna se é um invalido ou não
+
---
+++ aula 15/index.js
@@ -0,0 +1,10 @@
+let num1 = 9.54578;
+// let num2 = Math.floor(num1); // arredonda para baixo
+// let num2 = Math.ceil(num1); // arredonda para cima
+// let num2 = Math.round(num1); // arredonda para o mais próximo
+
+// console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,6)); // exibe o maior número da sequencia
+// console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,8,7,6)); // exibe o menor número da sequencia
+//const aleatorio = Math.round(Math.random() * (10 - 5) +5); // random exibe um número aleatorio e round arredonda
+
+console.log(aleatorio)
