580cb781fe3fda1a4312d2895695b11e374ba6f1
rfsantosr <rafaelhulk2@gmail.com>
2025-01-28 16:31:02 -0300

    aula 16

---
+++ aula 16/index.js
@@ -0,0 +1,44 @@
+//               0       1        2          3      4
+const alunos = ['Luiz', 'Maria', 'João']; // Luiza, Eduardo
+
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array);
+
+// alunos.push('Luiza');
+// alunos.push('Eduardo');
+// console.log(alunos.slice(0, 3));
+
+
+// console.log(alunos[50]); //Exibe um indice que ainda não tem, como undefined
+
+// delete alunos[1]; //remove o elemento e mantém o indixe
+// console.log(alunos);
+
+// const removido = alunos.shift(); //remove o primeiro indixe
+// console.log(removido);
+// console.log(alunos);
+
+// const removido = alunos.pop(); //remove o ultimo indixe
+// console.log(removido);
+// console.log(alunos);
+
+// alunos.unshift('Luiza'); // Adiciona no começo
+// alunos.unshift('Fábio'); // Adiciona no começo
+
+// alunos.push('Luiza'); // Adiciona no fim
+// alunos.push('Fábio'); // Adiciona no fim
+
+//alunos[alunos.length] = 'Luiza';
+//alunos[alunos.length] = 'Fábio';
+//alunos[alunos.length] = 'Luana';
+//console.log(alunos);
+
+
+
+//alunos[0] = 'Eduardo';
+//alunos [3] = 'Luiza';
+//console.log(alunos);
+//console.log(alunos[0]);
+//console.log(alunos[2]);
+
