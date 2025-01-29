a75cf9b813bd4c8aae579af4a1cfa0240ff01ee1
rfsantosr <rafaelhulk2@gmail.com>
2025-01-29 17:06:48 -0300

    commit

---
+++ aula 21/index.js
@@ -0,0 +1,16 @@
+/*
+Operadores de comparação
+> maior que
+>= maior que ou igual a
+< menor que
+<= menor que ou igual a 
+== igualdade (valor) (não recomendavel)
+=== igualdade estrita (valor e tipo)
+!= diferente (Só checa valor) (não recomendavel)
+!== diferente estrito (valor e tipo)
+*/
+
+const num1 = 10;
+const num2 = 11;
+const comp = num1 >= num2;
+console.log(comp);
---
+++ aula 22/index.js
@@ -0,0 +1,29 @@
+/*
+Operadores Lógicos
+&& -> AND -> E -> Todas as expressões precisam ser verdadeiras para retornar true
+|| -> OR -> OU
+! -> NOT -> NÃO
+*/
+
+
+
+console.log(!false); // nega o false e ele exibe true
+
+
+// console.log(!false); // nega o false e ele exibe true
+
+
+/*
+const usuario = 'Luiz'; // form que usuário digitou
+const senha = '123456';// form que usuário digitou
+//                     true                 false
+const vaiLogar = usuario === 'Luiz' && senha === '12345';
+console.log(vaiLogar);
+*/
+
+// const expressaoAnd = true && true && true && true;
+// const expressaoOr = false || false || true || false;;
+
+
+//const expressao = true && true && false && true;
+//console.log(expressao);
