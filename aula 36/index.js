31bc0346f172563a7af5f01747e7aa22c65f16b3
rfsantosr <rafaelhulk2@gmail.com>
2025-02-10 11:20:26 -0300

    aula 36

---
+++ aula 36/index.js
@@ -0,0 +1,25 @@
+// For in -> lê os índices ou chaves do objeto
+
+const pessoa = {
+    nome: 'Luiz',
+    sobrenome: 'Otávio',
+    idade: 30
+};
+
+for (let i in pessoa) {
+    console.log(pessoa[i]);
+}
+
+
+
+
+// const frutas = ['Pera', 'Maçã', 'Uva'];
+
+// for (let indice in frutas) {
+//     console.log(frutas[indice]);
+// }
+
+
+// for (let i = 0; i < frutas.length; i++) {
+//     console.log(frutas[i]);
+// }
