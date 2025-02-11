87c2172bf05a28c3d348382d24b27aa34a210b8e
rfsantosr <rafaelhulk2@gmail.com>
2025-02-11 14:32:59 -0300

    aula 38

---
+++ aula 37/aula 37.js
@@ -0,0 +1,28 @@
+const nome = ['Luiz', 'Otávio', 'Henrique'];
+
+for (let valor of nome) {
+    console.log(valor);
+}
+
+nome.forEach(function(valor){
+    console.log(valor);
+})
+
+
+
+
+
+
+// const nome = 'Luiz Otávio';
+
+// for (let i in nome){
+//     console.log(nome[i]);
+// }
+
+
+
+// const nome = 'Luiz Otávio';
+
+// for (let i = 0; i < nome.length; i++ ){
+//     console.log(nome[i]);
+// }
