4173c19c97b3aa558b3aaa8eab09e59fa97cdd59
rfsantosr <rafaelhulk2@gmail.com>
2025-03-01 11:56:15 -0300

    Exercicio

---
+++ Exercicios com IA/10/main.js
@@ -0,0 +1,17 @@
+const nome = document.getElementById('nome');
+const email = document.getElementById('email');
+const senha = document.getElementById('senha');
+const idade = document.getElementById('idade');
+const termo = document.getElementById('termo');
+const botao = document.getElementById('botao');
+
+botao.addEventListener('click', () => {
+    const nome = nomeInput.value;
+    const email = emailInput.value;
+    const senha = senhaInput.value;
+    const idade = idadeInput.value;
+})
+
+
+
+event.preventDefault()
