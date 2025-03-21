1a7b76805f4dc38e60bf9468fd3c86a2c01f6323
rfsantosr <rafaelhulk2@gmail.com>
2025-03-21 14:10:19 -0300

    aula 51

--- aula 51/main.js
+++ aula 51/main.js
@@ -1,9 +1,33 @@
-function falaFrase(comeco) {
-    function falaResto(resto) {
-        return comeco + ' ' + resto;
-    }
-    return falaResto;
+function duplica(n) {
+    return n * 2;
 }
 
-const olaMundo= falaFrase('Olá');
-console.log(olaMundo);
+function triplica(n) {
+    return n * 3;
+}
+
+function quadriplica(n) {
+    return n * 4;
+}
+
+console.log(duplica(2));
+console.log(triplica(2));
+console.log(quadriplica(2));
+
+
+
+
+
+
+
+
+
+// function falaFrase(comeco) {
+//     function falaResto(resto) {
+//         return comeco + ' ' + resto;
+//     }
+//     return falaResto;
+// }
+
+// const olaMundo= falaFrase('Olá');
+// console.log(olaMundo('mundo!'));
